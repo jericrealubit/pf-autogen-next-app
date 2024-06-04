0a4bc2b120e32ee210b7ce50e32872745252ef74
jeric realubit <jericrealubit@gmail.com>
2024-06-04 13:03:41 +1200

    show pdf preview in html

--- components/InputFile.tsx
+++ components/InputFile.tsx
@@ -7,30 +7,21 @@ import Image from "next/image";
 export function InputFile() {
   const [file, setFile] = useState<string>("");
   const [openAIResponce, setopenAIResponce] = useState<string>("");
-  let objbuilder = "";
+
+  let fileExtension = "";
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files === null) {
       window.alert("No file selected. Choose a file.");
     } else {
       const file = event.target.files[0];
+      fileExtension = file.name.split(".").reverse()[0]; // get file extension
 
       const reader = new FileReader();
       reader.readAsDataURL(file);
 
       reader.onload = () => {
         if (typeof reader.result === "string") {
-          objbuilder +=
-            '<object width="100%" height="100%" data="data:application/pdf;base64,';
-          objbuilder += file;
-          objbuilder += '" type="application/pdf" class="internal">';
-          objbuilder += '<embed src="data:application/pdf;base64,';
-          objbuilder += file;
-          objbuilder += '" type="application/pdf"  />';
-          objbuilder += "</object>";
-
-          console.log(file);
-          console.log(file.webkitRelativePath);
           console.log(reader.result);
           setFile(reader.result);
         }
@@ -45,6 +36,7 @@ export function InputFile() {
   return (
     <div className="grid w-full  items-center gap-1.5 p-5">
       <Input
+        className="cursor-pointer"
         id="picture"
         type="file"
         // accept=".pdf"
@@ -52,9 +44,8 @@ export function InputFile() {
       />
       {file !== "" ? (
         <div className="overflow-hidden">
-          {file?.type === "application/pdf" ? (
-            <iframe src={file} />
-          ) : (
+          <object data={file} width="600" height="300"></object>
+          {fileExtension !== "pdf" ?? (
             <Image
               src={file}
               className="w-full object-contain max-h-72"
@@ -66,7 +57,7 @@ export function InputFile() {
         </div>
       ) : (
         <div className="mb-4 p-8 text-center">
-          <p>Once yopu upload a file, you will see it here</p>
+          <p>Once you upload a file, you will see it here</p>
         </div>
       )}
     </div>
